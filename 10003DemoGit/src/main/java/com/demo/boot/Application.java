29d783e4d19a6661c8c6e689e920314992676a7a
Smita B Kumar <brijeshsmita@gmail.com>
2021-07-02 07:24:18 +0000

    Add files via upload
    initial commit

---
+++ 10003DemoGit/src/main/java/com/demo/boot/Application.java
@@ -0,0 +1,19 @@
+package com.demo.boot;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	
+}
